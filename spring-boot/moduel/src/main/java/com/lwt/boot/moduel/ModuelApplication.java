7906bdc7fc8587e61cc8f8ee5e9dda3a3940f928
KillerTwo <lwt2710@163.com>
2018-07-28 17:32:40 +0800

    新创建项目，第一次提交

---
+++ spring-boot/moduel/src/main/java/com/lwt/boot/moduel/ModuelApplication.java
@@ -0,0 +1,12 @@
+package com.lwt.boot.moduel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuelApplication.class, args);
+	}
+}
